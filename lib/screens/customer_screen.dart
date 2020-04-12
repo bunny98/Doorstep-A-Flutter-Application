f1d0849f90de84f7f2b7cc6c2db9f713ef1f6552
bunny98 <kavyansh.chourasia98@gmail.com>
2020-04-12 16:51:15 +0530

    Customer Screen

--- lib/screens/customer_screen.dart
+++ lib/screens/customer_screen.dart
@@ -1,6 +1,7 @@
 import 'package:doorstep/screens/login.dart';
 import 'package:doorstep/widgets/customer_map.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../providers/auth.dart';
@@ -13,17 +14,39 @@ class CustomerScreen extends StatefulWidget {
 }
 
 class _CustomerScreenState extends State<CustomerScreen> {
-  LatLng _target;
+  bool _buildMap = false;
+  bool _hasClickedMarker = false;
+  ShopData _tappedShop;
   TextStyle _headings =
       GoogleFonts.paprika(fontWeight: FontWeight.bold, fontSize: 15);
 
+  void _onMarkerTapped(ShopData tappedShop) {
+    print('*******' + tappedShop.address + '********');
+    setState(() {
+      _hasClickedMarker = true;
+      _tappedShop = tappedShop;
+    });
+  }
+
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero).then((_) {
-      _target = Provider.of<Auth>(context).getCurrLatLng;
-      Provider.of<Shops>(context)
-          .setCurrLoc(_target.latitude, _target.longitude);
-      Provider.of<Shops>(context).fetchShops();
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      AuthData user = Provider.of<Auth>(context).getAuthData;
+      Provider.of<Shops>(context).setCurrLoc(user.latitude, user.longitude);
+      Provider.of<Shops>(context).fetchShops().then((_) {
+        setState(() {
+          _buildMap = true;
+        });
+      });
+      setState(() {
+        _tappedShop = new ShopData(
+            address: user.address,
+            delivery: user.delivery,
+            latitude: user.latitude,
+            longitude: user.longitude,
+            typeOfShop: user.typeOfShop,
+            userId: user.userId);
+      });
     });
   }
 
@@ -31,51 +54,112 @@ class _CustomerScreenState extends State<CustomerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          padding: EdgeInsets.only(top: 40),
+          padding: EdgeInsets.only(top: 30),
+          height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(color: Colors.brown[50]),
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'Doorstep',
-                    style: GoogleFonts.pacifico(fontSize: 25),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Doorstep',
+                  style: GoogleFonts.pacifico(fontSize: 25),
+                ),
+                Row(children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.shopping_cart,
+                        size: 25,
+                      ),
+                      onPressed: () {}),
+                  IconButton(
+                      icon: Icon(
+                        Icons.exit_to_app,
+                        size: 25,
+                      ),
+                      onPressed: () async {
+                        await Provider.of<Auth>(context).signOut();
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(builder: (ctx) => LoginPage()),
+                            (_) => false);
+                      }),
+                ]),
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                // Padding(
+                //     padding: EdgeInsets.only(top: 20),
+                //     child: Text(
+                //       'Pick a Shop',
+                //       style: _headings,
+                //     )),
+                if (_buildMap)
+                  Container(
+                    // padding: EdgeInsets.only(top: 20, bottom: 20),
+                    height: MediaQuery.of(context).size.height - 78,
+                    decoration: BoxDecoration(
+                        border: Border(
+                            top: BorderSide(color: Colors.white, width: 2))),
+                    child: CustomerMap(
+                      onMarkerTapped: _onMarkerTapped,
+                    ),
+                  )
+                else
+                  Container(
+                    // padding: EdgeInsets.only(top: 20, bottom: 20),
+                    height: MediaQuery.of(context).size.height * 0.5,
+                    child: Center(child: CircularProgressIndicator()),
                   ),
-                  Row(children: [
-                    IconButton(
-                        icon: Icon(
-                          Icons.shopping_cart,
-                          size: 25,
+                if (_tappedShop != null)
+                  Positioned(
+                      bottom: 60,
+                      left: MediaQuery.of(context).size.width * 0.17,
+                      child: Center(
+                          child: Container(
+                        height: MediaQuery.of(context).size.height * 0.16,
+                        width: MediaQuery.of(context).size.height * 0.3,
+                        child: Card(
+                          color: Colors.pink[300],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          elevation: 8.0,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _hasClickedMarker
+                                    ? Text(
+                                        'Picked Shop: ' + _tappedShop.address,
+                                        style: GoogleFonts.aBeeZee(
+                                            color: Colors.white),
+                                      )
+                                    : Text(
+                                        'Your Location: ' + _tappedShop.address,
+                                        style: GoogleFonts.aBeeZee(
+                                            color: Colors.white)),
+                                _hasClickedMarker
+                                    ? Container(
+                                        child: RaisedButton.icon(
+                                            onPressed: () {},
+                                            icon: Icon(
+                                              Icons.add_shopping_cart,
+                                              color: Colors.black,
+                                            ),
+                                            label: Text(
+                                              'Order Now',
+                                              style: GoogleFonts.aBeeZee(
+                                                  color: Colors.black),
+                                            )))
+                                    : SizedBox(
+                                        height: 0,
+                                      ),
+                              ]),
                         ),
-                        onPressed: () {}),
-                    IconButton(
-                        icon: Icon(
-                          Icons.exit_to_app,
-                          size: 25,
-                        ),
-                        onPressed: () async {
-                          await Provider.of<Auth>(context).signOut();
-                          Navigator.of(context).pushAndRemoveUntil(
-                              MaterialPageRoute(builder: (ctx) => LoginPage()),
-                              (_) => false);
-                        }),
-                  ]),
-                ],
-              ),
-              Padding(
-                  padding: EdgeInsets.only(top: 50),
-                  child: Text(
-                    'Pick a Shop',
-                    style: _headings,
-                  )),
-              Container(
-                padding: EdgeInsets.only(top: 20, bottom: 20),
-                height: MediaQuery.of(context).size.height * 0.5,
-                child: CustomerMap(),
-              )
-            ],
-          )),
+                      )))
+              ],
+            )
+          ])),
     );
   }
 }
--- lib/widgets/customer_map.dart
+++ lib/widgets/customer_map.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:provider/provider.dart';
@@ -7,38 +8,87 @@ import '../providers/shops.dart';
 import '../providers/auth.dart';
 
 class CustomerMap extends StatefulWidget {
-  CustomerMap({Key key}) : super(key: key);
+  final void Function(ShopData) onMarkerTapped;
+  CustomerMap({Key key, this.onMarkerTapped}) : super(key: key);
   @override
   _CustomerMapState createState() => _CustomerMapState();
 }
 
 class _CustomerMapState extends State<CustomerMap> {
+  static CameraPosition _currLocCameraPostition;
   Location location = new Location();
   Completer<GoogleMapController> _controller = Completer();
-  static CameraPosition _currLocCameraPostition;
   LatLng _target;
+  List<ShopData> _nearByShops;
+  ShopData _tappedShop;
+  Set<Marker> _markers;
+
+  void _onMarkerTapped(MarkerId markerId) {
+    _tappedShop = _nearByShops[int.parse(markerId.value)];
+    widget.onMarkerTapped(_tappedShop);
+  }
+
+  void _setMarkers(List<ShopData> _shops) {
+    _markers = new Set();
+    for (int i = 0; i < _shops.length; i++) {
+      var _markerId = MarkerId('$i');
+      if (_shops[i].typeOfShop == 'Grocery Shop') {
+        _markers.add(Marker(
+          markerId: _markerId,
+          position: LatLng(_shops[i].latitude, _shops[i].longitude),
+          infoWindow: InfoWindow(title: _shops[i].address),
+          onTap: () {
+            _onMarkerTapped(_markerId);
+          },
+          icon: BitmapDescriptor.defaultMarkerWithHue(150),
+        ));
+      } else if (_shops[i].typeOfShop == 'Pharmacy') {
+        _markers.add(Marker(
+          markerId: _markerId,
+          position: LatLng(_shops[i].latitude, _shops[i].longitude),
+          infoWindow: InfoWindow(title: _shops[i].address),
+          onTap: () {
+            _onMarkerTapped(_markerId);
+          },
+          icon: BitmapDescriptor.defaultMarkerWithHue(50),
+        ));
+      } else {
+        _markers.add(Marker(
+          markerId: _markerId,
+          position: LatLng(_shops[i].latitude, _shops[i].longitude),
+          infoWindow: InfoWindow(title: _shops[i].address),
+          onTap: () {
+            _onMarkerTapped(_markerId);
+          },
+          icon: BitmapDescriptor.defaultMarkerWithHue(0),
+        ));
+      }
+    }
+  }
+
+  void _fetchShopsAndSetMarkers() {
+    _target = Provider.of<Auth>(context).getCurrLatLng;
+    _currLocCameraPostition = CameraPosition(target: _target, zoom: 15.5);
+    _nearByShops = Provider.of<Shops>(context).getNearbyShops;
+    _setMarkers(_nearByShops);
+  }
 
   @override
   Widget build(BuildContext context) {
-    _target = Provider.of<Auth>(context).getCurrLatLng;
-    _currLocCameraPostition = CameraPosition(target: _target, zoom: 15.5);
+    _fetchShopsAndSetMarkers();
     return Scaffold(
-      body: GoogleMap(
-        myLocationButtonEnabled: true,
-        myLocationEnabled: true,
-        compassEnabled: true,
-        mapType: MapType.normal,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-        initialCameraPosition: _currLocCameraPostition,
-        // markers: Set<Marker>.of(<Marker>[
-        //   Marker(
-        //     markerId: MarkerId('Curr Location'),
-        //     position: _target,
-        //   )
-        // ]),
-      ),
+      body: Stack(children: [
+        GoogleMap(
+          myLocationEnabled: true,
+          compassEnabled: true,
+          mapType: MapType.normal,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+          initialCameraPosition: _currLocCameraPostition,
+          markers: _markers,
+        ),
+      ]),
     );
   }
 }
